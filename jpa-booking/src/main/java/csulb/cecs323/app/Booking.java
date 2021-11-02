00e10a4b1c5048f49e70f0ccb413038e5c12b31b
John Apale <johnro.apale@gmail.com>
2021-11-01 17:22:44 -0700

    updated Booking class

--- jpa-booking/src/main/java/csulb/cecs323/app/Booking.java
+++ jpa-booking/src/main/java/csulb/cecs323/app/Booking.java
@@ -21,6 +21,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 /**
@@ -34,75 +35,127 @@ import java.util.logging.Logger;
  */
 public class Booking {
    /**
-    * You will likely need the entityManager in a great many functions throughout your application.
-    * Rather than make this a global variable, we will make it an instance variable within the CarClub
-    * class, and create an instance of CarClub in the main.
+    * Declaration of the EntityManager type variable called entityManager.
+    * Allow applications to manage and search for entities.
     */
    private EntityManager entityManager;
 
    /**
-    * The Logger can easily be configured to log to a file, rather than, or in addition to, the console.
-    * We use it because it is easy to control how much or how little logging gets done without having to
-    * go through the application and comment out/uncomment code and run the risk of introducing a bug.
-    * Here also, we want to make sure that the one Logger instance is readily available throughout the
-    * application, without resorting to creating a global variable.
+    * Declaration of the Logger type variable called LOGGER. Logs the program.
     */
    private static final Logger LOGGER = Logger.getLogger(Booking.class.getName());
 
    /**
-    * The constructor for the CarClub class.  All that it does is stash the provided EntityManager
-    * for use later in the application.
-    * @param manager    The EntityManager that we will use.
+    * The constructor for the Main class. Stashes the provided EntityManager for use
+    * later in the application.
+    * @param manager The EntityManager that we will use.
     */
    public Booking(EntityManager manager) {
       this.entityManager = manager;
    }
 
+   /**
+    * Creates a scanner to take user inputs
+    */
+   private static Scanner scan = new Scanner(System.in);
+
    public static void main(String[] args) {
       LOGGER.fine("Creating EntityManagerFactory and EntityManager");
       EntityManagerFactory factory = Persistence.createEntityManagerFactory("Booking");
       EntityManager manager = factory.createEntityManager();
-      // Create an instance of CarClub and store our new EntityManager as an instance variable.
+
+      // Create an instance of Books
       Booking booking = new Booking(manager);
-
-
-      // Any changes to the database need to be done within a transaction.
-      // See: https://en.wikibooks.org/wiki/Java_Persistence/Transactions
 
       LOGGER.fine("Begin of Transaction");
       EntityTransaction tx = manager.getTransaction();
 
       tx.begin();
-      // List of publishers that I want to persist.  I could just as easily done this with the seed-data.sql
-      List <Publishers> publishers = new ArrayList<>();
-      // Load up my List with the Entities that I want to persist.  Note, this does not put them
-      // into the database.
-//      publishers.add(new Publishers("John Apale", "714-888-8888", "johnro.apale@gmail.com"));
-//      publishers.add(new Publishers("Bloomsbury Publishing", "1111111111", "bloomsburypublishing@gmail.com"));
-//      publishers.add(new Publishers("Activision", "2222222222", "activision@gmail.com"));
-      // Create the list of publishers in the database.
-      booking.createEntity(publishers);
 
-      List <Authoring_Entities> auth_Entities = new ArrayList<>();
-//      auth_Entities.add(new Authoring_Entities("company1@gmail.com", "test1", "company1", "James Gunn", 2001));
-//      auth_Entities.add(new Authoring_Entities("company2@gmail.com", "test2", "company2", "Charles Park", 2002));
+//        List<Publishers> publishers = new ArrayList<>();
+//        List<> = new ArrayList<>();
+      boolean input = false;
+      while(!input){
+         System.out.println("Menu:\n1: Add a new Authoring Entity\n2: List info" +
+                 "\n3: Delete a Book\n4: Update a Book\n5: List all of the Primary Keys" +
+                 "\n6: Exit");
+         int user = scan.nextInt();
 
-      booking.createEntity(auth_Entities);
+         switch(user){
+            case 1:
 
+               break;
+            case 2:
 
-      // Commit the changes so that the new data persists and is visible to other users.
+               break;
+            case 3:
+
+               break;
+            case 4:
+
+               break;
+            case 5:
+
+               break;
+            case 6:
+               //Exit
+               input = true;
+               break;
+         }
+         //add new object
+         /*
+          * Writing Group
+          * Individual Author
+          * Ad Hoc Team
+          * An Individual Author to an existing Ad Hoc Team
+          */
+         //list info about an object
+         /*
+          * Publisher
+          * Book (include publisher and Authoring Entity
+          * Writing Group
+          */
+         //Delete a book
+         /*
+          * prompt all elements of a candidate key
+          * Make sure the book actually exists
+          */
+         //Update a book
+         /*
+          *Change the authoring entity for an existing book
+          */
+         //List the pk of all the rows
+         /*
+          * Publishers
+          * Books (show the title and ISBN)
+          * Authoring Entities (also show type of authoring entity)
+          */
+         //Exit
+      }
+
+      System.out.println("Interaction Complete");
       tx.commit();
-      LOGGER.fine("End of Transaction");
+      LOGGER.fine("End of Interaction");
 
-   } // End of the main method
+   }//End of main method
+
+
+   public void addObject(){
+      String entityName;
+      String email;
+      System.out.print("Enter the name of Authoring Entity: ");
+      entityName = scan.nextLine();
+      System.out.print("Enter the email of the Authoring Entity: ");
+      email = scan.nextLine();
+
+   }
 
    /**
     * Create and persist a list of objects to the database.
-    * @param entities   The list of entities to persist.  These can be any object that has been
-    *                   properly annotated in JPA and marked as "persistable."  I specifically
-    *                   used a Java generic so that I did not have to write this over and over.
+    * @param entities The list of entities to persist. These can be any object that has been
+    *                 properly annotated in JPA and marked as "persistable."
     */
-   public <E> void createEntity(List <E> entities) {
+   public <E> void createEntity(List<E> entities) {
       for (E next : entities) {
          LOGGER.info("Persisting: " + next);
          // Use the CarClub entityManager instance variable to get our EntityManager.
@@ -116,25 +169,5 @@ public class Booking {
          LOGGER.info("Persisted object after flush (non-null id): " + next);
       }
    } // End of createEntity member method
+}// End of Main class
 
-   /**
-    * Think of this as a simple map from a String to an instance of auto_body_styles that has the
-    * same name, as the string that you pass in.  To create a new Cars instance, you need to pass
-    * in an instance of auto_body_styles to satisfy the foreign key constraint, not just a string
-    * representing the name of the style.
-    * @param name       The name of the autobody style that you are looking for.
-    * @return           The auto_body_styles instance corresponding to that style name.
-    */
-//   public AuthoringEntity getStyle (String name) {
-//      // Run the native query that we defined in the auto_body_styles entity to find the right style.
-//      List<AuthoringEntity> styles = this.entityManager.createNamedQuery("",
-//              AuthoringEntity.class).setParameter(1, name).getResultList();
-//      if (styles.size() == 0) {
-//         // Invalid style name passed in.
-//         return null;
-//      } else {
-//         // Return the style object that they asked for.
-//         return styles.get(0);
-//      }
-//   }// End of the getStyle method
-} // End of Booking class
